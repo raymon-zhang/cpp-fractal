0faf4cd5b012277b8003ed0a0e804c36b05a6cca
Raymon Zhang <raymonzhang.rz@gmail.com>
2021-07-20 16:08:51 -0700

    Make mandelbrot take max iterations

--- cpp-fractal/src/Mandelbrot.cpp
+++ cpp-fractal/src/Mandelbrot.cpp
@@ -12,14 +12,14 @@ using namespace std;
 
 namespace rz {
 
-int Mandelbrot::getIterations(double x, double y) {
+int Mandelbrot::getIterations(double x, double y, int maxIterations) {
 	complex<double> z = 0;
 	complex<double> c(x, y);
 
 	int iterations = 0;
 
-	while (iterations < MAX_ITERATIONS) {
-		z = z*z  + c;
+	while (iterations < maxIterations) {
+		z = z * z + c;
 
 		if (abs(z) > 2) {
 			break;
