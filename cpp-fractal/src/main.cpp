8baa0ed708ba62dc6c4425541eb96672dc259954
Raymon Zhang <raymonzhang.rz@gmail.com>
2021-07-20 16:16:17 -0700

    Increase image resolution

--- cpp-fractal/src/main.cpp
+++ cpp-fractal/src/main.cpp
@@ -23,14 +23,14 @@ int main() {
 
 	Julia julia(-0.8, 0.156);
 //	Mandelbrot mandelbrot;
-	FractalCreator fractalCreator(800, 600, 1000, julia);
+	FractalCreator fractalCreator(1280, 720, 1000, julia);
 
 	fractalCreator.addRange(0.0, RGB(0, 0, 255));
 	fractalCreator.addRange(0.05, RGB(255, 99, 71));
 	fractalCreator.addRange(0.08, RGB(255, 215, 0));
 	fractalCreator.addRange(1.0, RGB(255, 255, 255));
 
-	fractalCreator.addZoom(Zoom(400, 300, 0.5));
+//	fractalCreator.addZoom(Zoom(400, 300, 0.5));
 //	fractalCreator.addZoom(Zoom(188, 268, 0.01));
 //	fractalCreator.addZoom(Zoom(312, 304, 0.1));
 
